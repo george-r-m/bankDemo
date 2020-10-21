5400ce9de73a58545319332c1479eab38af5c01c
George Roy <georgeroym98@gmail.com>
2020-10-21 16:05:14 +0530

    First commit for BankDemo

---
+++ BankAccount.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    class BankAccount
+    {
+        public string AccountNumber { get; }
+        public string Name { get; set; }
+
+        public decimal Balance
+        {
+            get
+            {
+
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        private static int seed = 111111111;
+        
+        public BankAccount(string name,decimal initialBalance)
+        {
+            Name = name;
+            Deposit(initialBalance, DateTime.Now, "First Deposit");
+            AccountNumber = seed.ToString();
+            seed++;
+        }
+        
+        public void Deposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount should be > 0");
+
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void Withdraw(decimal amount,DateTime date,string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount should be >0");
+            }
+
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("No suffiecient fund");
+            }
+            var withdraw = new Transaction(-amount, date, note);
+            allTransactions.Add(withdraw);
+        }
+
+        public string GetHistory()
+        {
+            var report = new StringBuilder();
+            report.Append("Date  Amount  Note");
+            foreach (var item in allTransactions)
+            {
+                report.Append($"\n{item.Date.ToShortDateString()} {item.Amount} {item.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("George",10000);
+            Console.WriteLine($"Account number= {account.AccountNumber} Account name={account.Name} Balance= {account.Balance}");
+            account.Deposit(50, DateTime.Now, "Depositing 50$");
+            Console.WriteLine($"\n {account.Balance}");
+            account.Withdraw(20, DateTime.Now, "Withdrawing 20");
+            Console.WriteLine($"\n {account.Balance}\n");
+            Console.WriteLine(account.GetHistory());
+        }
+    }
+}
